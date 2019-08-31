65329d0f4611b3a1d96ba6b96db9872d507b6c7f
Yoshiya Hinosawa <stibium121@gmail.com>
2019-08-31 09:34:24 +0900

    Update test code (#2)
    * Update .travis.yml
    
    Use shell installer instead of deprecated python installer.
    
    * Update test.ts
    
    * test: update test

--- test.ts
+++ test.ts
@@ -1,70 +1,71 @@
 // Adapted from https://github.com/zeit/ms/blob/master/test.js
 // Copyright (c) 2016 Zeit, Inc. MIT License
 // Copyright (c) 2018 Kevin "Kun" Kassimo Qian. MIT License
-import { test, assertEqual } from "https://deno.land/x/testing/testing.ts";
+import { test } from "https://deno.land/std/testing/mod.ts";
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { ms } from "./ms.ts";
 
 // ms(string)
 {
   test(function preserveMs() {
-    assertEqual(ms("100"), 100);
+    assertEquals(ms("100"), 100);
   });
 
   test(function mToMs() {
-    assertEqual(ms("1m"), 60000);
+    assertEquals(ms("1m"), 60000);
   });
 
   test(function hToMs() {
-    assertEqual(ms("1h"), 3600000);
+    assertEquals(ms("1h"), 3600000);
   });
 
   test(function dToMs() {
-    assertEqual(ms("2d"), 172800000);
+    assertEquals(ms("2d"), 172800000);
   });
 
   test(function wToMs() {
-    assertEqual(ms("3w"), 1814400000);
+    assertEquals(ms("3w"), 1814400000);
   });
 
   test(function sToMs() {
-    assertEqual(ms("1s"), 1000);
+    assertEquals(ms("1s"), 1000);
   });
 
   test(function msToMs() {
-    assertEqual(ms("100ms"), 100);
+    assertEquals(ms("100ms"), 100);
   });
 
   test(function decimals() {
-    assertEqual(ms("1.5h"), 5400000);
+    assertEquals(ms("1.5h"), 5400000);
   });
 
   test(function multiSpaces() {
-    assertEqual(ms("1   s"), 1000);
+    assertEquals(ms("1   s"), 1000);
   });
 
   test(function invalidRetNaN() {
-    assertEqual(isNaN(ms("☃") as number), true);
+    assertEquals(isNaN(ms("☃") as number), true);
   });
 
   test(function caseInsensitive() {
-    assertEqual(ms("1.5H"), 5400000);
+    assertEquals(ms("1.5H"), 5400000);
   });
 
   test(function numbersStartWithDot() {
-    assertEqual(ms(".5ms"), 0.5);
+    assertEquals(ms(".5ms"), 0.5);
   });
 
   test(function negativeInts() {
-    assertEqual(ms("-100ms"), -100);
+    assertEquals(ms("-100ms"), -100);
   });
 
   test(function negativeDecimals() {
-    assertEqual(ms("-1.5h"), -5400000);
-    assertEqual(ms("-10.5h"), -37800000);
+    assertEquals(ms("-1.5h"), -5400000);
+    assertEquals(ms("-10.5h"), -37800000);
   });
 
   test(function negativeDecimalsStartWithDot() {
-    assertEqual(ms("-.5h"), -1800000);
+    assertEquals(ms("-.5h"), -1800000);
   });
 }
 
@@ -72,144 +73,144 @@ import { ms } from "./ms.ts";
 
 {
   test(function millisecondsToMs() {
-    assertEqual(ms("53 milliseconds"), 53);
+    assertEquals(ms("53 milliseconds"), 53);
   });
 
   test(function msecsToMs() {
-    assertEqual(ms("17 msecs"), 17);
+    assertEquals(ms("17 msecs"), 17);
   });
 
   test(function secToMs() {
-    assertEqual(ms("1 sec"), 1000);
+    assertEquals(ms("1 sec"), 1000);
   });
 
   test(function minToMs() {
-    assertEqual(ms("1 min"), 60000);
+    assertEquals(ms("1 min"), 60000);
   });
 
   test(function hrToMs() {
-    assertEqual(ms("1 hr"), 3600000);
+    assertEquals(ms("1 hr"), 3600000);
   });
 
   test(function daysToMs() {
-    assertEqual(ms("2 days"), 172800000);
+    assertEquals(ms("2 days"), 172800000);
   });
 
   test(function longDecimals() {
-    assertEqual(ms("1.5 hours"), 5400000);
+    assertEquals(ms("1.5 hours"), 5400000);
   });
 
   test(function longNegativeIntegers() {
-    assertEqual(ms("-100 milliseconds"), -100);
+    assertEquals(ms("-100 milliseconds"), -100);
   });
 
   test(function longNegativeDecimals() {
-    assertEqual(ms("-1.5 hours"), -5400000);
+    assertEquals(ms("-1.5 hours"), -5400000);
   });
 
   test(function negativeDecimalsStartWithDot() {
-    assertEqual(ms("-.5 hr"), -1800000);
+    assertEquals(ms("-.5 hr"), -1800000);
   });
 }
 
 // ms(number, { long: true })
 {
   test(function longSupportMilliseconds() {
-    assertEqual(ms(500, { long: true }), "500 ms");
-    assertEqual(ms(-500, { long: true }), "-500 ms");
+    assertEquals(ms(500, { long: true }), "500 ms");
+    assertEquals(ms(-500, { long: true }), "-500 ms");
   });
 
   test(function longSupportSeconds() {
-    assertEqual(ms(1000, { long: true }), "1 second");
-    assertEqual(ms(1200, { long: true }), "1 second");
-    assertEqual(ms(10000, { long: true }), "10 seconds");
+    assertEquals(ms(1000, { long: true }), "1 second");
+    assertEquals(ms(1200, { long: true }), "1 second");
+    assertEquals(ms(10000, { long: true }), "10 seconds");
 
-    assertEqual(ms(-1000, { long: true }), "-1 second");
-    assertEqual(ms(-1200, { long: true }), "-1 second");
-    assertEqual(ms(-10000, { long: true }), "-10 seconds");
+    assertEquals(ms(-1000, { long: true }), "-1 second");
+    assertEquals(ms(-1200, { long: true }), "-1 second");
+    assertEquals(ms(-10000, { long: true }), "-10 seconds");
   });
 
   test(function longSupportMinutes() {
-    assertEqual(ms(60 * 1000, { long: true }), "1 minute");
-    assertEqual(ms(60 * 1200, { long: true }), "1 minute");
-    assertEqual(ms(60 * 10000, { long: true }), "10 minutes");
+    assertEquals(ms(60 * 1000, { long: true }), "1 minute");
+    assertEquals(ms(60 * 1200, { long: true }), "1 minute");
+    assertEquals(ms(60 * 10000, { long: true }), "10 minutes");
 
-    assertEqual(ms(-1 * 60 * 1000, { long: true }), "-1 minute");
-    assertEqual(ms(-1 * 60 * 1200, { long: true }), "-1 minute");
-    assertEqual(ms(-1 * 60 * 10000, { long: true }), "-10 minutes");
+    assertEquals(ms(-1 * 60 * 1000, { long: true }), "-1 minute");
+    assertEquals(ms(-1 * 60 * 1200, { long: true }), "-1 minute");
+    assertEquals(ms(-1 * 60 * 10000, { long: true }), "-10 minutes");
   });
 
   test(function longSupportHours() {
-    assertEqual(ms(60 * 60 * 1000, { long: true }), "1 hour");
-    assertEqual(ms(60 * 60 * 1200, { long: true }), "1 hour");
-    assertEqual(ms(60 * 60 * 10000, { long: true }), "10 hours");
+    assertEquals(ms(60 * 60 * 1000, { long: true }), "1 hour");
+    assertEquals(ms(60 * 60 * 1200, { long: true }), "1 hour");
+    assertEquals(ms(60 * 60 * 10000, { long: true }), "10 hours");
 
-    assertEqual(ms(-1 * 60 * 60 * 1000, { long: true }), "-1 hour");
-    assertEqual(ms(-1 * 60 * 60 * 1200, { long: true }), "-1 hour");
-    assertEqual(ms(-1 * 60 * 60 * 10000, { long: true }), "-10 hours");
+    assertEquals(ms(-1 * 60 * 60 * 1000, { long: true }), "-1 hour");
+    assertEquals(ms(-1 * 60 * 60 * 1200, { long: true }), "-1 hour");
+    assertEquals(ms(-1 * 60 * 60 * 10000, { long: true }), "-10 hours");
   });
 
   test(function longSupportDays() {
-    assertEqual(ms(24 * 60 * 60 * 1000, { long: true }), "1 day");
-    assertEqual(ms(24 * 60 * 60 * 1200, { long: true }), "1 day");
-    assertEqual(ms(24 * 60 * 60 * 10000, { long: true }), "10 days");
+    assertEquals(ms(24 * 60 * 60 * 1000, { long: true }), "1 day");
+    assertEquals(ms(24 * 60 * 60 * 1200, { long: true }), "1 day");
+    assertEquals(ms(24 * 60 * 60 * 10000, { long: true }), "10 days");
 
-    assertEqual(ms(-1 * 24 * 60 * 60 * 1000, { long: true }), "-1 day");
-    assertEqual(ms(-1 * 24 * 60 * 60 * 1200, { long: true }), "-1 day");
-    assertEqual(ms(-1 * 24 * 60 * 60 * 10000, { long: true }), "-10 days");
+    assertEquals(ms(-1 * 24 * 60 * 60 * 1000, { long: true }), "-1 day");
+    assertEquals(ms(-1 * 24 * 60 * 60 * 1200, { long: true }), "-1 day");
+    assertEquals(ms(-1 * 24 * 60 * 60 * 10000, { long: true }), "-10 days");
   });
 
   test(function longShouldRound() {
-    assertEqual(ms(234234234, { long: true }), "3 days");
+    assertEquals(ms(234234234, { long: true }), "3 days");
 
-    assertEqual(ms(-234234234, { long: true }), "-3 days");
+    assertEquals(ms(-234234234, { long: true }), "-3 days");
   });
 }
 
 // ms(number)
 {
   test(function supportMilliseconds() {
-    assertEqual(ms(500), "500ms");
+    assertEquals(ms(500), "500ms");
 
-    assertEqual(ms(-500), "-500ms");
+    assertEquals(ms(-500), "-500ms");
   });
 
   test(function supportSeconds() {
-    assertEqual(ms(1000), "1s");
-    assertEqual(ms(10000), "10s");
+    assertEquals(ms(1000), "1s");
+    assertEquals(ms(10000), "10s");
 
-    assertEqual(ms(-1000), "-1s");
-    assertEqual(ms(-10000), "-10s");
+    assertEquals(ms(-1000), "-1s");
+    assertEquals(ms(-10000), "-10s");
   });
 
   test(function supportMinutes() {
-    assertEqual(ms(60 * 1000), "1m");
-    assertEqual(ms(60 * 10000), "10m");
+    assertEquals(ms(60 * 1000), "1m");
+    assertEquals(ms(60 * 10000), "10m");
 
-    assertEqual(ms(-1 * 60 * 1000), "-1m");
-    assertEqual(ms(-1 * 60 * 10000), "-10m");
+    assertEquals(ms(-1 * 60 * 1000), "-1m");
+    assertEquals(ms(-1 * 60 * 10000), "-10m");
   });
 
   test(function supportHours() {
-    assertEqual(ms(60 * 60 * 1000), "1h");
-    assertEqual(ms(60 * 60 * 10000), "10h");
+    assertEquals(ms(60 * 60 * 1000), "1h");
+    assertEquals(ms(60 * 60 * 10000), "10h");
 
-    assertEqual(ms(-1 * 60 * 60 * 1000), "-1h");
-    assertEqual(ms(-1 * 60 * 60 * 10000), "-10h");
+    assertEquals(ms(-1 * 60 * 60 * 1000), "-1h");
+    assertEquals(ms(-1 * 60 * 60 * 10000), "-10h");
   });
 
   test(function supportDays() {
-    assertEqual(ms(24 * 60 * 60 * 1000), "1d");
-    assertEqual(ms(24 * 60 * 60 * 10000), "10d");
+    assertEquals(ms(24 * 60 * 60 * 1000), "1d");
+    assertEquals(ms(24 * 60 * 60 * 10000), "10d");
 
-    assertEqual(ms(-1 * 24 * 60 * 60 * 1000), "-1d");
-    assertEqual(ms(-1 * 24 * 60 * 60 * 10000), "-10d");
+    assertEquals(ms(-1 * 24 * 60 * 60 * 1000), "-1d");
+    assertEquals(ms(-1 * 24 * 60 * 60 * 10000), "-10d");
   });
 
   test(function shouldRound() {
-    assertEqual(ms(234234234), "3d");
+    assertEquals(ms(234234234), "3d");
 
-    assertEqual(ms(-234234234), "-3d");
+    assertEquals(ms(-234234234), "-3d");
   });
 }
 
@@ -246,5 +247,5 @@ test(function invalidInputs() {
   } catch (e) {
     errCount++;
   }
-  assertEqual(errCount, 5);
+  assertEquals(errCount, 5);
 });
